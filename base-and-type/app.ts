a030dff336b1e5197a2d8527ca6d7f9171c04ea3
ygs <ygs3004@naver.com>
2026-04-11 22:08:11 +0900

    section 2: TypeScript 기본 & Type - number, string, boolean

--- base-and-type/app.ts
+++ base-and-type/app.ts
@@ -1,17 +1,22 @@
-function add(n1: number, n2: number) {
-
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
     // js case
-    if(typeof n1 !== "number" || typeof n2 !== "number") {
-        throw new Error("Incorrect input")
-    }
+    // if(typeof n1 !== "number" || typeof n2 !== "number") {
+    //     throw new Error("Incorrect input")
+    // }
 
-    return n1 + n2;
+    const result = n1 + n2;
+    if(showResult) {
+        console.log(phrase + result);
+    } else {
+        return n1 + n2;
+    }
 }
 
 // const number1 = "5";
 const number1 = 5;
 const number2 = 2.8;
+const printResult = true;
+const resultPhrase = "Result: ";
 
-const result = add(number1, number2);
 
-console.log(result);
+add(number1, number2, printResult, resultPhrase);
