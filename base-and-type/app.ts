a825af8da7ab3aea89b74b8ebb9361306cf8760b
ygs <ygs3004@naver.com>
2026-04-17 23:58:49 +0900

    section 2: TypeScript 기본 & Type - void return type

--- base-and-type/app.ts
+++ base-and-type/app.ts
@@ -1,30 +1,9 @@
-type Combinable = number | string;
-type ConversionDescriptor = "as-number" | "as-text";
-
-function combine(
-    input1: Combinable,
-    input2: Combinable,
-    resultConversion: ConversionDescriptor
-) {
-    let result;
-    if (typeof input1 === "number" && typeof input2 === "number" || resultConversion === "as-number") {
-        result = +input1 + +input2;
-    } else {
-        result = input1.toString() + input2.toString();
-    }
-    if (resultConversion === "as-number") {
-        return +result;
-    } else {
-        result = result.toString()
-    }
-    return result;
+function add(n1: number, n2: number): number {
+    return n1 + n2;
 }
 
-const combinedAges = combine(30, 26, "as-number");
-console.log(combinedAges);
+function printResult(num: number): void { // undefined 반환 됨
+    console.log("Result: " + num);
+}
 
-const combinedStringAges = combine("30", "26", "as-number");
-console.log(combinedStringAges);
-
-const combinedNames = combine("Faker", "Choby", "as-text");
-console.log(combinedNames);
+printResult(add(5, 12))
