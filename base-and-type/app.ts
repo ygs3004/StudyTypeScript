b54e378e4934ce0c5dde8a9a69b2a6093f9ce23a
ygs <ygs3004@naver.com>
2026-04-18 22:20:50 +0900

    section 2: TypeScript 기본 & Type - function type

--- base-and-type/app.ts
+++ base-and-type/app.ts
@@ -7,3 +7,13 @@ function printResult(num: number): void { // undefined 반환 됨
 }
 
 printResult(add(5, 12))
+
+// let combineValues: Function // 함수의 변수 및 리턴 타입 오류를 적절히 잡아낼 수 없음
+let combineValues: (a: number, b: number) => number;
+combineValues = add;
+
+// 컴파일 오류
+// combineValues = 5;
+// combineValues = printResult();
+
+console.log(combineValues(8 ,8))
