097b0d720053470141aa5c44dbdf01b4e58b6284
ygs <ygs3004@naver.com>
2026-04-19 23:50:34 +0900

    section 2: TypeScript 기본 & Type - never type

--- base-and-type/app.ts
+++ base-and-type/app.ts
@@ -8,3 +8,13 @@ userInput = "Ygs";
 if(typeof userInput === "string"){
   userName = userInput; // 타입 체크 후에 사용시 컴파일 오류 발생 안함
 }
+
+function generateError(message: string, code: number): never { // return 값이 없는 type 초기 타입스크립트에는 없었음, void 의 경우 undefined
+    // 오류를 던지는 경우
+    throw {message: message, errorCode: code};
+
+    // 무한재귀함수의 경우
+    // while (true) {}
+}
+
+generateError("An error occurred", 500);
