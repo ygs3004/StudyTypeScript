b5aa30d2ac1e6171cabc0a2816261321c080356f
ygs <ygs3004@naver.com>
2026-04-28 23:37:37 +0900

    section 3: TypeScript 컴파일러 - tsconfig, noImplicitReturns, noUnusedLocals

--- compiler/src/app.ts
+++ compiler/src/app.ts
@@ -1,6 +1,16 @@
+let appId = "123" // 전역변수 noUnusedLocals 무관
 const button = document.querySelector(".button");
 
+function add(n1: number, n2: number) {
+    if (n1 + n2 < 0) {
+        return n1 + n2;
+    }
+
+    // return; noImplicitReturns 시 리턴 함수는 항상 리턴 필요
+}
+
 function clickHandler(message: string) {
+    // let userName = "Ygs"; , noUnusedLocals 체크
     console.log("clicked: " + message);
 }
 
