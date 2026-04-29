c18706d34c3e629b4adbc13b99180f8c4e85f28e
ygs <ygs3004@naver.com>
2026-04-29 23:35:46 +0900

    section 4: 모던 javascript & typescript - let, const, var

--- mordern-js-and-ts/src/app.ts
+++ mordern-js-and-ts/src/app.ts
@@ -1,1 +1,18 @@
-// Code goes here!
+const userName = "YGS";
+// userName = "Yoon Gun Soo"
+
+let age = 30;
+age = 29;
+
+function add(a: number, b: number) {
+    let result;
+    result = a + b;
+    return result;
+}
+
+
+// js 기준 코드 작동, ts 기준 컴파일 에러
+// if (age > 20) {
+//     var isOld = true;
+// }
+// console.log(isOld);
