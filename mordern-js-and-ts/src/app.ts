fbd03c2283c3b4ad6583adab5e2928419d67f461
ygs <ygs3004@naver.com>
2026-04-30 23:36:06 +0900

    section 4: 모던 javascript & typescript - typescript arrow function

--- mordern-js-and-ts/src/app.ts
+++ mordern-js-and-ts/src/app.ts
@@ -1,18 +1,35 @@
+// let & const
 const userName = "YGS";
 // userName = "Yoon Gun Soo"
 
 let age = 30;
 age = 29;
 
-function add(a: number, b: number) {
+function add2(a: number, b: number) {
     let result;
     result = a + b;
     return result;
 }
-
 
 // js 기준 코드 작동, ts 기준 컴파일 에러
 // if (age > 20) {
 //     var isOld = true;
 // }
 // console.log(isOld);
+
+
+// arrow function
+const add = (a: number, b: number) => a + b;
+
+console.log(add(2, 5));
+
+// @ts-ignore
+const printOutput: (a: number | string) => void = output => console.log(output);
+
+const button = document.querySelector("button");
+
+if (button) {
+    button.addEventListener("click", event => console.log(event));
+}
+
+printOutput(add(5, 2));
