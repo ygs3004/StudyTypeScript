81d61059de93726dd317c7d744c9b6e852238cc1
ygs <ygs3004@naver.com>
2026-05-06 23:57:52 +0900

    section 4: 클래스 & 인터페이스 - 클래스 this

--- class-and-interface/src/app.ts
+++ class-and-interface/src/app.ts
@@ -4,8 +4,14 @@ class Department {
     constructor(n: string) {
         this.name = n;
     }
+
+    describe(this: Department) { // 현재 객체를 참조하는 것을 명시하여 컴파일 시점에서 오류 방지
+        console.log("Department: " + this.name);
+    }
 }
 
 const accounting = new Department("Accounting");
+accounting.describe();
 
-console.log(accounting);
+const accountingCopy = { name: "other", describe: accounting.describe };
+accountingCopy.describe();
