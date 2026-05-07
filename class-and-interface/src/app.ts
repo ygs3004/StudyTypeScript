97c4aa4b26cf1cfcf9b0886fd81d4faf62dee0cd
ygs <ygs3004@naver.com>
2026-05-07 22:54:49 +0900

    section 4: 클래스 & 인터페이스 - public, private

--- class-and-interface/src/app.ts
+++ class-and-interface/src/app.ts
@@ -1,5 +1,6 @@
 class Department {
-    name: string;
+    public name: string;
+    private employees: string[] = [];
 
     constructor(n: string) {
         this.name = n;
@@ -8,10 +9,27 @@ class Department {
     describe(this: Department) { // 현재 객체를 참조하는 것을 명시하여 컴파일 시점에서 오류 방지
         console.log("Department: " + this.name);
     }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee);
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
 
 const accounting = new Department("Accounting");
-accounting.describe();
 
-const accountingCopy = { name: "other", describe: accounting.describe };
-accountingCopy.describe();
+accounting.addEmployee("Faker");
+accounting.addEmployee("Keria");
+
+// accounting.employees[2] = "Oner"; // private 접근 제한자로 접근을 막을 수 있다
+
+accounting.describe();
+accounting.name = "NEW NAME"; // public 접근 제한자는 접근 가능
+accounting.printEmployeeInformation();
+
+// const accountingCopy = { name: "other", describe: accounting.describe };
+// accountingCopy.describe();
