cc368901c71c7873e5df75774cb15074bbac992d
ygs <ygs3004@naver.com>
2026-05-22 23:38:38 +0900

    section 6: 고급 타입 - dom type casting

--- advanced-types/src/app.ts
+++ advanced-types/src/app.ts
@@ -105,3 +105,10 @@ function moveAnimal(animal: Animal) {
 }
 
 moveAnimal({type: "bird", flyingSpeed: 10});
+
+// const userInputElement = <HTMLInputElement>document.getElementById("user-input");
+const userInputElement = document.getElementById("user-input") as HTMLInputElement;
+
+if(userInputElement) {
+    userInputElement.value = "Hi there!";
+}
