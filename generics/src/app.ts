72d7f3e4148a340a7da79cc1a0eee8b02bba68d3
ygs <ygs3004@naver.com>
2026-05-28 22:42:04 +0900

    section 7: 제네릭 - generic function

--- generics/src/app.ts
+++ generics/src/app.ts
@@ -1,12 +1,19 @@
-const names: Array<string> = [];
-// names[0].split("") // generic으로 string 타입 이므로 string split 메서드 사용가능
+// const names: Array<string> = [];
+// // names[0].split("") // generic으로 string 타입 이므로 string split 메서드 사용가능
+//
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve("timeout");
+//     }, 2000)
+// });
+//
+// promise.then(data => {
+//     data.split("") // generic으로 string 타입 이므로 string split 메서드 사용가능
+// })
 
-const promise: Promise<string> = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve("timeout");
-    }, 2000)
-});
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+    return Object.assign(objA, objB);
+}
 
-promise.then(data => {
-    data.split("") // generic으로 string 타입 이므로 string split 메서드 사용가능
-})
+const mergedObj = merge<{name:string, hobbies: string[]}, {age: number}>({name: "Ygs", hobbies: ["Game"]}, {age: 30});
+console.log(mergedObj);
