14699cc131840e8da2f9f0c9453fa9ab3f1bf0fe
ygs <ygs3004@naver.com>
2026-05-31 00:18:45 +0900

    section 7: 제네릭 - generic utility - Partial, Readonly

--- generics/src/app.ts
+++ generics/src/app.ts
@@ -73,3 +73,21 @@ const numberStorage = new DataStorage<number>();
 // objStorage.removeItem(yoonObj);
 // console.log(objStorage.getItems());
 
+interface CourseGoal {
+    title: string;
+    description: string;
+    completeUntil: Date;
+}
+
+function createCourseGoal(title: string, description: string, date: Date): CourseGoal {
+    let courseGoal: Partial<CourseGoal> = {};
+    courseGoal.title = title;
+    courseGoal.description = description;
+    courseGoal.completeUntil = date;
+
+    return courseGoal as CourseGoal;
+}
+
+const names: Readonly<string[]> = ["Faker", "Keria"];
+// names.push("Peyz"); error
+// names.pop(); error
