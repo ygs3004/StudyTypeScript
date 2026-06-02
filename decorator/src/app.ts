31603b780c0d466b8fb5501bef7c81c8edeb4f30
ygs <ygs3004@naver.com>
2026-06-02 23:21:26 +0900

    section 7: 데코레이터 - decorator example

--- decorator/src/app.ts
+++ decorator/src/app.ts
@@ -1,1 +1,20 @@
 // tsconfig.json 옵션 "experimentalDecorators": true, 추가 필수
+
+// 데코레이터
+function Logger(constructor: Function) {
+    console.info("Logging....");
+    console.log(constructor);
+}
+
+@Logger
+class Person {
+    name = "Ygs";
+
+    constructor() {
+        console.log("Create Person.....");
+    }
+}
+
+const person = new Person();
+
+console.log(person);
