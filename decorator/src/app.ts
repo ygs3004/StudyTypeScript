90b9fe8bcc8aa2aedeed6a5c6572d2261fa53e3c
ygs <ygs3004@naver.com>
2026-06-04 23:35:37 +0900

    section 7: 데코레이터 - property decorator

--- decorator/src/app.ts
+++ decorator/src/app.ts
@@ -40,3 +40,31 @@ class Person {
 const person = new Person();
 
 console.log(person);
+
+function Log(target: any, propertyName: string | Symbol) {
+    console.log("Property Decorator");
+    console.log(target, propertyName)
+}
+
+class Product{
+    @Log
+    title: string;
+    private _price: number;
+
+    set price(val: number) {
+        if (val > 0) {
+            this._price = val;
+        }else{
+            throw new Error("Invalid price")
+        }
+    }
+
+    constructor(t: string) {
+        this.title = t;
+        this._price = 0;
+    }
+
+    getPriceWithTax(tax: number) {
+        return this._price * (1 + tax);
+    }
+}
