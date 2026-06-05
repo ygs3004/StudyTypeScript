e68c99b4bb428cd2dc17a6d58ab7f05ee8f5a386
ygs <ygs3004@naver.com>
2026-06-05 23:53:43 +0900

    section 7: 데코레이터 - decorator: accessor & method & parameter

--- decorator/src/app.ts
+++ decorator/src/app.ts
@@ -46,11 +46,33 @@ function Log(target: any, propertyName: string | Symbol) {
     console.log(target, propertyName)
 }
 
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+    console.log("Accessor Decorator");
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+
+function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
+    console.log("Method Decorator");
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+
+function Log4(target: any, name: string | Symbol, position: number) {
+    console.log("Parameter  Decorator");
+    console.log(target);
+    console.log(name);
+    console.log(position);
+}
+
 class Product{
     @Log
     title: string;
     private _price: number;
 
+    @Log2
     set price(val: number) {
         if (val > 0) {
             this._price = val;
@@ -64,7 +86,8 @@ class Product{
         this._price = 0;
     }
 
-    getPriceWithTax(tax: number) {
+    @Log3
+    getPriceWithTax(@Log4 tax: number) {
         return this._price * (1 + tax);
     }
 }
