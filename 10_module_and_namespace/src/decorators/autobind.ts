12e04af33d3f38b834ace6b58dbb60503b72ccea
ygs <ygs3004@naver.com>
2026-06-30 02:47:31 +0900

    section 10: module and namespace - es module import export

--- 10_module_and_namespace/src/decorators/autobind.ts
+++ 10_module_and_namespace/src/decorators/autobind.ts
@@ -1,15 +1,13 @@
-namespace App {
-    // autobind decorator
-    export function autobind(_target: any, _methodName: string, descriptor: PropertyDescriptor) {
-        const originalMethod = descriptor.value;
-        const adjDescriptor: PropertyDescriptor = {
-            configurable: true,
-            get(){
-                const boundFn = originalMethod.bind(this);
-                return boundFn;
-            }
-        };
+// autobind decorator
+export function autobind(_target: any, _methodName: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
 
-        return adjDescriptor;
-    }
+    return adjDescriptor;
 }
