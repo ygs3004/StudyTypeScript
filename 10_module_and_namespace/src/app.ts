e9f3610dde00823bc650cf20f45a7e77f62631d7
ygs <ygs3004@naver.com>
2026-06-29 21:44:09 +0900

    section 10: module and namespace - 네임스페이스 정리

--- 10_module_and_namespace/src/app.ts
+++ 10_module_and_namespace/src/app.ts
@@ -1,9 +1,4 @@
 // namespace 를 위해 아래와 같이 /// 이후 작성
-/// <reference path="models/drag-drop.ts" />
-/// <reference path="models/project.ts" />
-/// <reference path="state/project-state.ts" />
-/// <reference path="util/validation.ts" />
-/// <reference path="decorators/autobind.ts" />
 /// <reference path="components/project-input.ts" />
 /// <reference path="components/project-list.ts" />
 
