ab90182221b6baf4b63ce75596aa170cae363f73
ygs <ygs3004@naver.com>
2026-07-01 21:29:45 +0900

    section 10: module and namespace - named export

--- 10_module_and_namespace/src/components/base-component.ts
+++ 10_module_and_namespace/src/components/base-component.ts
@@ -1,5 +1,7 @@
+export const something = "...";
+
 // Component Base Class
-export abstract class Component<T extends HTMLElement, U extends HTMLElement> {
+export default abstract class Component<T extends HTMLElement, U extends HTMLElement> {
     templateElement: HTMLTemplateElement;
     hostElement: T;
     element: U;
