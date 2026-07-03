c781c0e84b1a8d39c4c36e7eb9bab5616738e8aa
ygs <ygs3004@naver.com>
2026-07-03 21:54:24 +0900

    section 11: webpack - webpack config

--- 11_webpack/src/state/project-state.ts
+++ 11_webpack/src/state/project-state.ts
@@ -1,5 +1,5 @@
 // Project State Manage
-import {Project, ProjectStatus} from "../models/project.js";
+import {Project, ProjectStatus} from "../models/project";
 
 type Listener<T> = (items: T[]) => void
 
