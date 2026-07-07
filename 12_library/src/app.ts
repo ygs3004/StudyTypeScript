179f83a06a562f4aa9bb6e4b94ee88d42a474120
ygs <ygs3004@naver.com>
2026-07-07 23:00:45 +0900

    section 12: library - class-transformer

--- 12_library/src/app.ts
+++ 12_library/src/app.ts
@@ -1,7 +1,18 @@
-import _ from "lodash";
+import "reflect-metadata";
+import {plainToClass} from "class-transformer";
 
-declare var GLOBAL: string;
+import {Product} from "./product.model";
 
-console.log(_.shuffle(([1, 2, 3])))
+const products = [
+    {title: "A Carpet", price: 29.99},
+    {title: "A Book", price: 10.99}
+];
 
-console.log(GLOBAL);
+// const loadedProducts = products.map(prod => {
+//     return new Product(prod.title, prod.price);
+// });
+const loadedProducts = plainToClass(Product, products);
+
+for(const prod of loadedProducts) {
+    console.log(prod.getInformation());
+}
