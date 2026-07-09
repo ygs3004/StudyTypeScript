e14629368cd66d97d5557d87e2e7d14871c4e995
ygs <ygs3004@naver.com>
2026-07-09 13:35:14 +0900

    section 12: library - class-validator

--- 12_library/src/app.ts
+++ 12_library/src/app.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
 import {plainToClass} from "class-transformer";
+import {validate} from "class-validator";
 
 import {Product} from "./product.model";
 
@@ -7,6 +8,17 @@ const products = [
     {title: "A Carpet", price: 29.99},
     {title: "A Book", price: 10.99}
 ];
+
+const newProd = new Product("", -5);
+validate(newProd).then(errors => {
+    if(errors.length > 0) {
+        console.log("VALIDATION ERRORS!");
+        console.log(errors);
+    }else{
+        console.log(newProd.getInformation());
+    }
+})
+console.log(newProd.getInformation());
 
 // const loadedProducts = products.map(prod => {
 //     return new Product(prod.title, prod.price);
