c3f781a7a090fb19c89acb537ed4e464fa8da2c3
ygs <ygs3004@naver.com>
2026-07-14 19:17:03 +0900

    section 13: project-google-map-pos - openlayers rendering

--- 13_lib_prj_google_map/src/app.ts
+++ 13_lib_prj_google_map/src/app.ts
@@ -1,35 +1,24 @@
-import axios from "axios";
-
 const form = document.querySelector("form")!;
-const addressInput = document.getElementById("address")! as HTMLInputElement;
-
-const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY!;
-
-type GoogleGeocodingResponse = {
-    results: { geometry: { location: { lat: number; lng: number } } }[];
-    status: "OK" | "ZERO_RESULTS";
-};
+declare var ol: any;
 
 function searchAddressHandler(event: Event) {
     event.preventDefault();
-    const enteredAddress = addressInput.value;
 
-    axios
-        .get<GoogleGeocodingResponse>(
-            `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
-                enteredAddress
-            )}&key=${GOOGLE_API_KEY}`
-        )
-        .then(response => {
-            if (response.data.status !== "OK") {
-                throw new Error("Could not fetch location!");
-            }
-            // const coordinates = response.data.results[0].geometry.location;
+    const coordinates = {lat: 40.41, lng: -73.99}; // Google API에서 좌표를 가져올 수 없음, 더미 API 사용
+
+    document.getElementById('map')!.innerHTML = ''; // <div id="map">에서 <p> 제거
+    new ol.Map({
+        target: 'map',
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.OSM()
+            })
+        ],
+        view: new ol.View({
+            center: ol.proj.fromLonLat([coordinates.lng, coordinates.lat]),
+            zoom: 16
         })
-        .catch(err => {
-            alert(err.message);
-            console.log(err);
-        });
+    });
 }
 
 form.addEventListener("submit", searchAddressHandler);
