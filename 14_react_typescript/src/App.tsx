46735d87a13e16b0d78b1ad9e9dfb7b250f6710f
ygs <ygs3004@naver.com>
2026-07-16 15:56:47 +0900

    section 14: react-typescript - props, type

--- 14_react_typescript/src/App.tsx
+++ 14_react_typescript/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import TodoList from "./components/TodoList";
 
 const App: React.FC = () => {
+    const todos = [{id: "t1", text: "Finish the course"}];
   return (
       <div className="App">
-111
+        <TodoList items={todos}/>
       </div>
   );
 }
