eca0fd9a20f2fa38e0863e6cee912f8dcd63e52d
ygs <ygs3004@naver.com>
2026-07-19 16:29:48 +0900

    section 14: react-typescript - useRef, form input

--- 14_react_typescript/src/App.tsx
+++ 14_react_typescript/src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import TodoList from "./components/TodoList";
+import NewTodo from "./components/NewTodo";
 
 const App: React.FC = () => {
     const todos = [{id: "t1", text: "Finish the course"}];
-  return (
-      <div className="App">
-        <TodoList items={todos}/>
-      </div>
-  );
+    return (
+        <div className="App">
+            <NewTodo/>
+            <TodoList items={todos}/>
+        </div>
+    );
 }
 
 export default App;
