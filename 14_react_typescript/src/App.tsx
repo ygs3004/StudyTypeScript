1d9445a29f62441f9112ee16c25bc207199ea408
ygs <ygs3004@naver.com>
2026-07-22 23:53:02 +0900

    section 14: react-typescript - useState, 상태관리

--- 14_react_typescript/src/App.tsx
+++ 14_react_typescript/src/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TodoList from "./components/TodoList";
 import NewTodo from "./components/NewTodo";
+import {Todo} from "./todo.model";
 
 const App: React.FC = () => {
-    const todos = [{id: "t1", text: "Finish the course"}];
+    const [todos, setTodos] = useState<Todo[]>([]);
 
     const todoAddHandler = (text: string) => {
-        console.log(text);
+        setTodos(prevTodos => [...prevTodos, {id: Math.random().toString(), text:text}])
     }
 
     return (
