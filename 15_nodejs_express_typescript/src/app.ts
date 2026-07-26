d156903569cab2974bbcaf38e5fd9620a26b5340
ygs <ygs3004@naver.com>
2026-07-26 22:35:27 +0900

    section 15: nodejs & express - typescript - expiress, type

---
+++ 15_nodejs_express_typescript/src/app.ts
@@ -0,0 +1,12 @@
+import express, {NextFunction, Request, Response} from "express";
+import todoRoutes from "./routes/todos";
+
+const app = express();
+
+app.use("/todos", todoRoutes);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    res.status(500).json({message: err.message});
+});
+
+app.listen(3000);
---
+++ 15_nodejs_express_typescript/src/routes/todos.ts
@@ -0,0 +1,13 @@
+import {Router} from "express";
+
+const router = Router();
+
+router.post("/");
+
+router.get("/", );
+
+router.patch("/");
+
+router.delete("/");
+
+export default router;
