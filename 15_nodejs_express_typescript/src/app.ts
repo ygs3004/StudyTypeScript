8d1a8e336a65afd9507f72087aa24459c16340aa
ygs <ygs3004@naver.com>
2026-07-28 00:10:44 +0900

    section 15: nodejs & express - typescript - controller, post

--- 15_nodejs_express_typescript/src/app.ts
+++ 15_nodejs_express_typescript/src/app.ts
@@ -1,7 +1,10 @@
 import express, {NextFunction, Request, Response} from "express";
 import todoRoutes from "./routes/todos";
+import {json} from "body-parser";
 
 const app = express();
+
+app.use(json());
 
 app.use("/todos", todoRoutes);
 
---
+++ 15_nodejs_express_typescript/src/controllers/todos.ts
@@ -0,0 +1,13 @@
+import {RequestHandler} from "express";
+import {Todo} from "../models/todo";
+
+const TODOS: Todo[] = [];
+
+export const createTodo: RequestHandler = (req, res, next) => {
+    const text= (req.body as {text:string}).text;
+    const newTodo = new Todo(Math.random().toString(), text);
+
+    TODOS.push(newTodo);
+
+    res.status(201).json({message: "Create the todo.", createdTodo: newTodo});
+}
