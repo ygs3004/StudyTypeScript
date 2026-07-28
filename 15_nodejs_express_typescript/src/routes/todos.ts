fcf09b502cdd9d63b06cec9d5b9bdfd47c72b103
ygs <ygs3004@naver.com>
2026-07-28 23:04:07 +0900

    section 15: nodejs & express - typescript - crud

--- 15_nodejs_express_typescript/src/routes/todos.ts
+++ 15_nodejs_express_typescript/src/routes/todos.ts
@@ -1,14 +1,14 @@
 import {Router} from "express";
-import {createTodo} from "../controllers/todos";
+import {createTodo, deleteTodo, getTodos, updateTodo} from "../controllers/todos";
 
 const router = Router();
 
 router.post("/", createTodo);
 
-router.get("/", );
+router.get("/", getTodos);
 
-router.patch("/");
+router.patch("/:id", updateTodo);
 
-router.delete("/");
+router.delete("/:id", deleteTodo);
 
 export default router;
